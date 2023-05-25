e3eb18eb92132770b002da4b064916845448ae03
22015248 <22015248@LAPTOP-17AD0DSF>
2023-05-25 11:49:52 +0800

    first push to P00 repository in GitHub

--- P00/Test/CalculatorTest.java
+++ P00/Test/CalculatorTest.java
@@ -25,52 +25,8 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testAdd() {
-		//fail("Not yet implemented");
-		int a  = 1234;
-		int b = 8765;
-		
-		Calculator cal = new Calculator();
-		int actual = cal.add(a, b);
-		
-		int expected = 9999;
-		assertEquals (expected,actual);
+	public void test() {
+		fail("Not yet implemented");
 	}
-	@Test
-	public void testsubtract() {
-		int a = 4321;
-		int b = 1234;
-			
-		Calculator cal = new Calculator();
-		int actual = cal.subtract(a, b); 
-			 
-		int expected = 3087;
-		assertEquals (expected, actual);
-		}
-	
-	@Test
-	public void testMultiply() {
-		int a = 2;
-		int b = 2;
-			
-		Calculator cal = new Calculator();
-		int actual = cal.multiply(a, b); 
-			 
-		int expected = 4;
-		assertEquals (expected, actual);
-		}
-	@Test
-	public void testDivide() {
-		int a = 2;
-		int b = 2;
-			
-		Calculator cal = new Calculator();
-		int actual = cal.divide(a, b); 
-			 
-		int expected = 1;
-		assertEquals (expected, actual);
-		}
-
-
 
 }
